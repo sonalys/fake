0b2af659de530b640c1986b473da39af162227bb
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com.>
2024-03-13 08:17:58 +0100

    modify test to use tempDir

--- generate_test.go
+++ generate_test.go
@@ -1,14 +1,16 @@
 package fake
 
 import (
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func Test_Generate(t *testing.T) {
-	Run([]string{"testdata"}, "testdata/out", []string{"testdata/out"})
+	output := t.TempDir()
+	Run([]string{"testdata"}, output, []string{})
 	g := NewGenerator("mocks")
-	_, err := g.ParseFile("testdata/out/testdata/stub.gen.go")
+	_, err := g.ParseFile(path.Join(output, "testdata", "stub.gen.go"))
 	require.NoError(t, err)
 }
